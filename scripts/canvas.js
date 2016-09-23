99c5b44d1b2b087c17484874338d5f20245af59e
srikasip <srikasip@gmail.com>
2016-09-22 19:54:10 -0700

    updates all around

--- scripts/canvas.js
+++ scripts/canvas.js
@@ -1,9 +1,18 @@
-function handleDefaultText(sentTxt){
-  alert("stuff");
+function handleDefaultText(){
+  $(".txt_coder_interaction").val($(".txt_coder_interaction").attr("default_text"));
 }
 
 $(document).ready(function(){
-  $(".txt_coder_interaction").focus(function(){
-    handleDefaultText($(this));
+  handleDefaultText();
+  $('.txt_coder_interaction').focus(function(){
+    if ($(this).val() == $(this).attr("default_text")){
+      $(this).val("");
+    }
   });
+  $('.txt_coder_interaction').focusout(function(){
+    if ($.trim($(this).val()) == ""){
+      $(this).val($(this).attr("default_text"));
+    }
+  });
+
 });
